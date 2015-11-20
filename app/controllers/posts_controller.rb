e4c0a63a7535bc85307f406cead54df59e9dba5b
yatsu <user@ElitesCamp.local>
2015-11-21 02:06:29 +0900

    def editとdef updateの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,15 @@ class PostsController < ApplicationController
     @post.save
     redirect_to "/posts/#{@post.id}"
   end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to "/posts/#{@post.id}"
+  end
+
 
 private
   def post_params
