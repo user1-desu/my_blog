aecb1b05cfa3247dfe0d2492059b86928b80d360
yatsu <user@ElitesCamp.local>
2015-11-21 02:15:35 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120163525) do
+ActiveRecord::Schema.define(version: 20151120171450) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "image_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
